c918fa399db05ddd3e4605d6108eb4aa8d6d71b1
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-03 14:36:52 -0400

    further documentary clean-up

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,10 +91,20 @@ function displayTodayInHeader() {
 }
 
 function loadSavedCalendarInfo() {
+  // Gets persisted calendar information from localStorage (key = "calendar-info")
+  // and uses it to populate the textarea contents for the hours represented
   calendarInfo = JSON.parse(localStorage.getItem("calendar-info"));
   if (calendarInfo === null) {
+    // If no previously stored calendar-info item was stored then start with
+    // an empty array.
     calendarInfo = [];
   }
+  // Each element in the calendarInfo array will represent an hour with 
+  // recorded information.  Note that not all hours will have array elements.
+  // For each such element, the "key" field is the hour of the day (numeric, 0-23)
+  // and the "content" field is the content for the corresponding textarea.
+  // Note that the stored array elements can be in any order and are not expected
+  // to be chronological.
   for (let i=0;i<calendarInfo.length;i++) {
     let entry = calendarInfo[i];
     let numKey = entry.key;
@@ -115,6 +125,9 @@ function addHourListListeners() {
   if (hourList !== null) {
     hourList.addEventListener("click",function(event) {
       let elem = event.target;
+      // If a click is detected on a save button or its child <i> element then
+      // we call saveForHourKey using the value derived from the data-idx attribute.
+      // e.g. if data-idx = 9 then our key will be "hour-9".
       if (elem.matches(".saveBtn") || elem.matches(".saveBtn>i")) {
         let hourKey = "hour-" + elem.getAttribute("data-idx");
         saveForHourKey(hourKey);
@@ -122,8 +135,11 @@ function addHourListListeners() {
     })
     hourList.addEventListener("change" ,function(event) {
       let elem = event.target;
+      // If a change is detected in one of the textarea elements then we
+      // change its style so that it is visually apparent that the element
+      // has been changed but not yet saved.
       if (elem.matches("textarea")) {
-        let idx = elem.getAttribute("data-idx");
+        // let idx = elem.getAttribute("data-idx");  // TODO - delete this?
         elem.classList.add("unsaved");
       }
     })
@@ -131,7 +147,13 @@ function addHourListListeners() {
 }
 
 function saveForHourKey(hKey) {
-  let div = document.querySelector("#"+hKey);
+  // Given an hKey parameter value which is of the form "hour-H" where H is a numeric hour of the
+  // day, this functions saves the corresponding textarea content combined with the hour
+  // in an object of the form {key:<hour>, content:<textarea content>} in the calendarInfo
+  // array.  In doing so, care is taken not to duplicate entries for any given hour.
+  //
+  // Get the relevant <div> element based on the id= value 
+  let div = document.querySelector("#"+hKey);  // TODO - do we even need these first two lines?
   if (div !== null) {
     let txtArea = document.querySelector("#"+hKey + " textarea");
     content = txtArea.value;
@@ -139,6 +161,8 @@ function saveForHourKey(hKey) {
     if (numKey !== null) {
       let matchingEntryFound = false;
       let matchedIdx = -1;
+      // Scan all the existing entries in the calendarInfo array to determine if information for
+      // the specified hour is already in that array.
       for (i=0;i<calendarInfo.length&&(!matchingEntryFound);i++) {
         matchingEntryFound = (calendarInfo[i].key === numKey);
         if (matchingEntryFound) {
@@ -146,11 +170,17 @@ function saveForHourKey(hKey) {
         }
       }
       if (matchingEntryFound) {
+        // If the hour in question was already found in the array then update the
+        // content thereof ONLY if it is DIFFERENT from what is already recorded.  If NOT
+        // different then there is actually no change, and therefore, no need to update
+        // localStorage.
         if (content !== calendarInfo[matchedIdx].content) {
           calendarInfo[matchedIdx].content = content;
           localStorage.setItem("calendar-info",JSON.stringify(calendarInfo));
         }  // else no change and no need to save
       } else {
+        // If no match was found in the array then we need to add an new element to the array,
+        // but only if the content to be added is not blank.
         if (content !== "") {
           calendarInfo.push({key:numKey,content:content});
           localStorage.setItem("calendar-info",JSON.stringify(calendarInfo));
